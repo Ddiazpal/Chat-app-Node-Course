31dd0d6123fb1cb2efcfebea9339dcb07238bbd1
ddiazpal <ddiazpal@unbosque.edu.co>
2018-06-15 14:18:39 -0500

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -15,14 +15,13 @@ app.use(express.static(publicpath));
 io.on('connection', function (socket)  {   //Register an event listener -- listen for specific event
     console.log('New user connected');
 
-    socket.emit('newMessage', { // creating the event
-        from: "John",
-        text: "Hey, How are you?",
-        createAt: 123 
-    }); 
-
     socket.on('createMessage', (newMessage) => {
         console.log('Message created', newMessage);
+        io.emit('newMessage', {  //emits an evnet to every single connection
+            from: newMessage.from,
+            text: newMessage.text,
+            createdAt: new Date().getTime()
+        }); 
     });
 
     socket.on('disconnect', function ()  {
