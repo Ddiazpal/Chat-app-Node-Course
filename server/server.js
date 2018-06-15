b712ce1b61e5e42e66beef77d4ed625e7bfe1af0
ddiazpal <ddiazpal@unbosque.edu.co>
2018-06-15 15:04:53 -0500

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -15,13 +15,31 @@ app.use(express.static(publicpath));
 io.on('connection', function (socket)  {   //Register an event listener -- listen for specific event
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app'
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
+
     socket.on('createMessage', (newMessage) => {
         console.log('Message created', newMessage);
+
         io.emit('newMessage', {  //emits an evnet to every single connection
             from: newMessage.from,
             text: newMessage.text,
             createdAt: new Date().getTime()
         }); 
+
+     /*   socket.broadcast.emit('newMessage', {
+            from: newMessage.from,
+            text: newMessage.text,
+            createdAt: new Date().getTime()
+        });*/
     });
 
     socket.on('disconnect', function ()  {
