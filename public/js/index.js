c9ecd209a5698a83afca08ee07ded8899a51075b
ddiazpal <ddiazpal@unbosque.edu.co>
2018-06-29 16:18:55 -0500

    Format timestamps using moment.js

--- public/js/index.js
+++ public/js/index.js
@@ -9,18 +9,19 @@ socket.on('disconnect', function ()  {
 });
 
 socket.on('newMessage', function (message) {
-    console.log('New Message', message);
+    var formattedTime = moment(message.createdAt).format('h:mm a');
     var li = jQuery('<li></li>');
-    li.text(`${message.from}:  ${message.text}`);
+    li.text(`${message.from} ${formattedTime}:  ${message.text}`);
 
     jQuery('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function (message) {
     var li = jQuery('<li></li>');
+    var formattedTime = moment(message.createdAt).format('h:mm a');
     var a= jQuery('<a target="_blank">My current location</a>'); //here comes the link text
 
-    li.text(`${message.from}: `);
+    li.text(`${message.from} ${formattedTime}: `);
     a.attr('href', message.url); //fetch the value
     li.append(a);
     jQuery('#messages').append(li);
